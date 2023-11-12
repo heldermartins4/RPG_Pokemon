a215b4b0a12fad21679cf726aad971326e637655
heldermartins4 <eusouhelder123@gmail.com>
2023-11-12 16:12:03 -0300

    add/set sprite of player

--- src/main/java/controllers/game/entity/Player.java
+++ src/main/java/controllers/game/entity/Player.java
@@ -1,9 +1,8 @@
 package controllers.game.entity;
 
-import java.awt.Color;
+// import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -20,7 +19,7 @@ public class Player extends Entity {
         this.gp = gp;
         this.key = key;
         
-        // getImagePlayer();
+        getImagePlayer();
     }
 
     public void setDefaultValues(int x, int y, int width, int height, int speed) {
@@ -35,15 +34,17 @@ public class Player extends Entity {
 
     public void getImagePlayer() {
 
+        final String relative_path = "/sprites/sprites_char_walk/";
+
         try {
-            up1 = ImageIO.read(new File("/src/main/resources/sprites/sprites_char_walk/char_left (1).png"));
-            up2 = ImageIO.read(new File("/src/main/resources/sprites/sprites_char_walk/char_left (2).png"));
-            down1 = ImageIO.read(new File("/resources/sprites/sprites_char_walk/char_down (1).png"));
-            down2 = ImageIO.read(new File("/src/main/resources/sprites/sprites_char_walk/char_down (2).png"));
-            left1 = ImageIO.read(new File("/src/main/resources/sprites/sprites_char_walk/char_left (1).png"));
-            left2 = ImageIO.read(new File("/src/main/resources/sprites/sprites_char_walk/char_left (2).png"));
-            right1 = ImageIO.read(new File("/src/main/resources/sprites/sprites_char_walk/char_right (1).png"));
-            right2 = ImageIO.read(new File("/src/main/resources/sprites/sprites_char_walk/char_right (2).png"));
+            up1 = ImageIO.read(getClass().getResource(relative_path + "char_up_1.png"));
+            up2 = ImageIO.read(getClass().getResource(relative_path + "char_up_2.png"));
+            down1 = ImageIO.read(getClass().getResource(relative_path + "char_down_1.png"));
+            down2 = ImageIO.read(getClass().getResource(relative_path + "char_down_2.png"));
+            left1 = ImageIO.read(getClass().getResource(relative_path + "char_left_1.png"));
+            left2 = ImageIO.read(getClass().getResource(relative_path + "char_left_2.png"));
+            right1 = ImageIO.read(getClass().getResource(relative_path + "char_right_1.png"));
+            right2 = ImageIO.read(getClass().getResource(relative_path + "char_right_2.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -52,42 +53,45 @@ public class Player extends Entity {
     public void update() {
         if (key.up) {
             y -= speed;
+            direction = "up";
         }
         if (key.down) {
             y += speed;
+            direction = "down";
         }
         if (key.left) {
             x -= speed;
+            direction = "left";
         }
         if (key.right) {
             x += speed;
+            direction = "right";
         }
     }
 
     public void draw(Graphics2D g) {
 
-        g.setColor(Color.white);
-        g.fillRect(x, y, gp.tile_size, gp.tile_size);
+        // g.setColor(Color.white);
+        // g.fillRect(x, y, gp.tile_size, gp.tile_size);
 
-        // BufferedImage img = null; // set default image
+        BufferedImage img = null; // set default image
 
         // set image based on direction
-        // switch (direction) {
-        //     case "up":
-        //         img = up1;
-        //         break;
-        //     case "down":
-        //         img = down1;
-        //         break;
-        //     case "left":
-        //         img = left1;
-        //         break;
-        //     case "right":
-        //         img = right1;
-        //         break;
-        // }
+        switch (direction) {
+            case "up":
+                img = up1;
+                break;
+            case "down":
+                img = down1;
+                break;
+            case "left":
+                img = left1;
+                break;
+            case "right":
+                img = right1;
+                break;
+        }
 
-        // g.drawImage(img, x, y, gp.tile_size, gp.tile_size, null);
-        // System.out.println(getClass().getResource("/resources/sprites/sprites_char_walk/char_down (1).png").getFile());
+        g.drawImage(img, x, y, gp.tile_size, gp.tile_size, null);
     }
 }
